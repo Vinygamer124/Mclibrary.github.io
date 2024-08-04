6431a06dda5d0250e02376ed42ba82a16c538e41
Vinygamer124 <vinysub57@gmail.com>
2024-08-04 21:53:01 +0700

    Create scripts.js
    Signed-off-by: Vinygamer124 <vinysub57@gmail.com>

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = "block";
+}
+
+function closePopup(popupId) {
+    document.getElementById(popupId).style.display = "none";
+}
+
+function togglePassword(fieldId) {
+    const field = document.getElementById(fieldId);
+    field.type = (field.type === 'password')
