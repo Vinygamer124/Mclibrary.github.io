ef100015d710557963e70028bd313858e8cde08a
Vinygamer124 <vinysub57@gmail.com>
2024-08-08 21:21:00 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,3 @@
-function openPopup(popupId) {
-    document.getElementById(popupId).style.display = "block";
-}
-
-function closePopup(popupId) {
-    document.getElementById(popupId).style.display = "none";
-}
-
-function togglePassword(fieldId) {
-    const field = document.getElementById(fieldId);
-    field.type = (field.type === 'password') ? 'text' : 'password';
-}
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    this.classList.toggle('active');
+});
